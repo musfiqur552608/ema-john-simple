b0630d7070a3d30204c735a89f42f6513cc3d4f0
musfiqur552608 <mushfiqur15-10872@diu.edu.bd>
2022-09-26 23:04:58 +0600

    Product component design done

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,11 +2,17 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    const{name, img, seller, price, ratings} = props.product;
+    const { name, img, seller, price, ratings } = props.product;
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <p className='product-name'>{name}</p>
+            <div className="product-info">
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Rating: {ratings} stars</small></p>
+            </div>
+            <button className='btn-cart'><p>Add to Cart</p></button>
         </div>
     );
 };
