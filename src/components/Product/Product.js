e310fefcc3fed3688d7de37b81de37419cbc4383
musfiqur552608 <mushfiqur15-10872@diu.edu.bd>
2022-09-27 12:48:03 +0600

    some calculations

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
+    const {handleAddToCart} = props;
     const { name, img, seller, price, ratings } = props.product;
     return (
         <div className='product'>
@@ -12,7 +13,7 @@ const Product = (props) => {
                 <p><small>Seller: {seller}</small></p>
                 <p><small>Rating: {ratings} stars</small></p>
             </div>
-            <button className='btn-cart'><p>Add to Cart</p></button>
+            <button onClick={()=> props.handleAddToCart(props.product)} className='btn-cart'><p>Add to Cart</p></button>
         </div>
     );
 };
